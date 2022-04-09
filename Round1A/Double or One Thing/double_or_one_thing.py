56f986bdaf28b136f99d930eb44e0584fcacc8c2
batuhanbat <64826454+batuhanbat@users.noreply.github.com>
2022-04-09 16:33:16 +0300

    create py
    .

---
+++ Round1A/Double or One Thing/double_or_one_thing.py
@@ -0,0 +1,13 @@
+def double_or_not(S):
+    return_string = ""
+    for elem in S[::-1]:
+        return_string = min(elem + return_string, elem + elem + return_string)
+    return return_string
+
+# I/O Code
+T = int(input())
+
+for case in range(1, T + 1):   
+    S = str(input().strip())
+    result = double_or_not(S)
+    print('Case #{}: {}'.format(case, result))
